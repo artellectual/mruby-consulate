699783022aa2b85b77e9b2ab7d9c4bf347359f51
Zack Siri <zack@artellectual.com>
2017-11-01 08:40:21 +0000

    clean up service methods

--- mrblib/consulate/base.rb
+++ mrblib/consulate/base.rb
@@ -12,5 +12,21 @@ module Consulate
 
       @http = Consulate.http
     end
+
+    protected
+
+    def render(response)
+      if response['content-type'] == 'application/json'
+        JSON.parse(response['body'])
+      else
+        response['body']
+      end
+    end
+
+    def parameterize(hash)
+      '?' + hash.map do |data|
+        "#{data.first}=#{data.last}"
+      end.join('&')
+    end
   end
 end
--- mrblib/consulate/catalog.rb
+++ mrblib/consulate/catalog.rb
@@ -7,7 +7,15 @@ module Consulate
       # Lists out services
       url = format('%s/%s', base_url, 'services'.freeze)
 
-      JSON.parse(http.get(url)['body'])
+      render http.get(url)
+    end
+
+    def service(name, options = {})
+      url = format(
+        '%s/%s/%s', base_url, 'service'.freeze, name
+      )
+
+      render http.get(url + parameterize(options))
     end
   end
 end
