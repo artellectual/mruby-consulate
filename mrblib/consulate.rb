eab24eb626f55f5bd52a426bff91ef7c2de4c390
Zack Siri <zack@codemy.net>
2017-11-08 15:51:44 +0700

    we can now query for services with tags

--- mrblib/consulate.rb
+++ mrblib/consulate.rb
@@ -1,6 +1,16 @@
 # Consulate Module
 module Consulate
   class << self
-    # Public module functions go here
+    def catalog
+      @catalog ||= Consul::Catalog.new
+    end
+
+    def get_ip_for(service_name, domain)
+      ips = catalog.service(service_name, tag: domain).map do |n|
+        n['TaggedAddresses']['wan']
+      end
+
+      ips.first
+    end
   end
 end
